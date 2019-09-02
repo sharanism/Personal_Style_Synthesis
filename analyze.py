08b0371b42a678a5075df52fbb348f2fe57709fe
huji520shahar <shahar.jacob@mail.huji.ac.il>
2019-09-03 01:01:31 +0300

    Adding time analyze methods

--- analyze.py
+++ analyze.py
@@ -50,13 +50,23 @@ def get_stripes(data):
     return stripe_list
 
 
-def calc_dist(old_x, new_x, old_y, new_y):
+def calc_dist_2D(old_x, new_x, old_y, new_y):
     """
-    :return: the distance between two points
+    :return: the distance between two points - 2D
     """
     return math.sqrt(float(pow(new_x - old_x, 2)) + float(pow(new_y - old_y, 2)))
 
 
+def calc_dist_1D(old_point, new_point):
+    """
+    :return: the distance between two points - 1D
+    """
+    return abs(float(new_point) - float(old_point))
+
+
+####################
+# Geometric length #
+####################
 def calc_dist_of_stripe(x_array, y_array):
     """
     :param x_array: array of all the x values
@@ -65,7 +75,7 @@ def calc_dist_of_stripe(x_array, y_array):
     """
     dist = 0
     for i in range(1, len(x_array)):
-        dist += calc_dist(x_array[i-1], x_array[i], y_array[i-1], y_array[i])
+        dist += calc_dist_2D(x_array[i-1], x_array[i], y_array[i-1], y_array[i])
 
     return dist
 
@@ -82,7 +92,52 @@ def calc_file_dist(stripes_list):
     return int(dist)
 
 
+def calc_dist_average_of_stripe(stripes_list):
+    return float(calc_file_dist(stripes_list)) / float(len(stripes_list))
 
 
-stripe_list = get_stripes(get_data_file("data/week1/D_01/aliza/aliza__130319_0935_D_01.txt"))
-print(calc_file_dist(stripe_list))
+###############
+# Time length #
+###############
+def calc_time_of_stripe(time_array):
+    time = 0
+    for i in range(1, len(time_array)):
+        time += calc_dist_1D(float(time_array[i - 1]), float(time_array[i]))
+
+    return time
+
+
+def calc_file_time(stripes_list):
+    time = 0
+    for stripe in stripes_list:
+        time += calc_time_of_stripe(list(stripe['time']))
+
+    return time
+
+
+def calc_time_average_of_stripe(stripes_list):
+    return float(calc_file_time(stripes_list)) / float(len(stripes_list))
+
+
+#########
+# Tests #
+#########
+input1 = "data/week1/D_01/aliza/aliza__130319_0935_D_01.txt"
+input2 = "data/week1/D_01/zoey/zoey__130319_1208_D_01.txt"
+stripe_list1 = get_stripes(get_data_file(input1))
+stripe_list2 = get_stripes(get_data_file(input2))
+
+print("In Dubby picture: ")
+print()
+
+print("Total geometric length of Aliza: " + str(calc_file_dist(stripe_list1)))
+print("Average geometric length of Aliza: " + str(calc_dist_average_of_stripe(stripe_list1)))
+print("Total time of Aliza: " + str(calc_file_time(stripe_list1)))
+print("Average geometric length of Aliza: " + str(calc_time_average_of_stripe(stripe_list1)))
+
+print()
+
+print("Total geometric length of Zoey: " + str(calc_file_dist(stripe_list2)))
+print("Average Geometric length of Zoey: " + str(calc_dist_average_of_stripe(stripe_list2)))
+print("Total time of Zoey: " + str(calc_file_time(stripe_list2)))
+print("Average geometric length of Zoey: " + str(calc_time_average_of_stripe(stripe_list2)))
